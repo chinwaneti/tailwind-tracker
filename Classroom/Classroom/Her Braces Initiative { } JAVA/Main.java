f15a8f6a35c39df055b82f6addec135cfcbd006a
monielover <chidinmanwaneti.cn@gmail.com>
2023-04-17 10:11:31 -0700

    feat:add files

---
+++ Classroom/Classroom/Her Braces Initiative { } JAVA/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+       int a = 6 + 6;
+       float b = (float) (3.6 + 3.65);
+        Double c = 2.2, d = 3.4;
+        double e = c + d;
+       char f = '4';
+       //boolean
+        if (2 == 2);
+            System.out.println("true");
+
+
+
+
+       System.out.println(a);
+       System.out.println(b);
+       System.out.println(e);
+       System.out.println(f);
+
+    }
+}
---
+++ Classroom/Classroom/Her Braces Initiative { } JAVA/main 1.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        double base = 5;
+        double height = 6;
+        int = 1/2 * base * height;
+        double a = 0.5 * 5 * 6;
+
+
+        double sumDouble(int a = 3, int b = 4, int c = 5);
+        double d = (double) a + b + c;
+
+
+        System.out.println(d);
+
+
+       System.out.println(a);
+
+
+
+
+    }
+}
