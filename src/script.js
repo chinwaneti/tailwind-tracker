014e4e6a3fd52241d9151c74e1ec7bb1d2b2314c
monielover <chidinmanwaneti.cn@gmail.com>
2023-04-18 21:06:09 -0700

    feat:add files

---
+++ src/script.js
@@ -0,0 +1,13 @@
+const animal = 'fox'
+const fish1 = 'snake'
+const fish2 = 'jelly'
+const fish3 = 'shark'
+
+const fish = document.querySelectorAll('.fish')
+const animal1 = document.getElementById('fox')
+
+animal1.textContent = animal
+
+fish[0].textContent = fish1
+fish[1].textContent = fish2
+fish[2].textContent = fish3
